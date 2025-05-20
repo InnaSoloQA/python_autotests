f353716abedc4e3bb43f98bd1c3c9d8067603022
InnaSoloQA <vralm@mail.ru>
2025-05-20 15:46:53 +0300

    Add files via upload

--- PythonProjects/RequestsTest/main.py
+++ PythonProjects/RequestsTest/main.py
@@ -5,25 +5,28 @@ TOKEN = '712c3f7837286ec7588ea32798abd7ea'
 HEADER = {'Content-Type':'application/json', 'trainer_token':TOKEN}
 
 body_create = {
-    "name": "Бульбазавр",
-    "photo_id": "1"
+    "name": "generate",
+    "photo_id": "-1"
 }
 
+response_create = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_create)
+print(response_create.text)
+
+response_data = response_create.json()
+pokemon_id = response_data.get("id")
+
 body_name = {
-    "pokemon_id": "316460",
+    "pokemon_id": pokemon_id,
     "name": "Mimi",
     "photo_id": 2
 }
 
+response_name = requests.put(url = f'{URL}/pokemons', headers = HEADER, json = body_name)
+print(response_name.text)
+
 body_pokaball = {
-    "pokemon_id": "316460"
+    "pokemon_id": pokemon_id
 }
-
-'''response_create = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_create)
-print(response_create.text)'''
-
-"""response_name = requests.put(url = f'{URL}/pokemons', headers = HEADER, json = body_name)
-print(response_name.text)"""
 
 response_pokeball = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_name)
 print(response_pokeball.text)
